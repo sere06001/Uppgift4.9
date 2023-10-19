cb7c6bffe87f9b614e052457b6190da62b11471b
sere06001 <sere06001@utb.vaxjo.se>
2023-10-19 11:17:33 +0200

    Add project files.

---
+++ Uppgift 4.9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom.Compiler;
+
+namespace Uppgift4_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programmet kommer att rita ut en rätvinklig triangel.");
+            Console.WriteLine("Bestäm sidlängden på triangeln.");
+            int längd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < längd; i++)
+            {
+                string tecken = new string('*', i+1);
+                Console.WriteLine(tecken);
+            }
+        }
+    }
+}
